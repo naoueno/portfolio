1820762fe86ce6788407c8bd8860d5ff90d8f92a
上野奈於 <nao@uenonaonoMacBook-Pro.local>
2020-04-16 18:20:05 +0900

    20200416

--- src/components/skillchart-front.vue
+++ src/components/skillchart-front.vue
@@ -10,17 +10,12 @@ export default {
         datasets: [
           {
             label: 'Frontend',
-            data: [3, 3, 2, 2, 2],
+            data: [3, 3, 1.5, 2, 0.5],
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
+              'rgba(181, 26, 26, 0.75)',
             ],
             borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+              'rgba(181, 26, 26, 0.75)'
             ],
             borderWidth: 1
           },
