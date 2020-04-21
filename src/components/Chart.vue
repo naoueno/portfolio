5d2fbbaba47b8606cebaac31ba5b108f366386da
上野奈於 <nao@uenonaonoMacBook-Pro.local>
2020-04-21 18:13:00 +0900

    20200421

---
+++ src/components/Chart.vue
@@ -0,0 +1,24 @@
+<script>
+import FrontChart from './skillchart-front.vue';
+import BackChart from './skillchart-back.vue';
+import DevChart from './skillchart-develop.vue';
+
+export default {
+  components: {
+      FrontChart,
+      BackChart,
+      DevChart,
+    },
+  computed:{
+    ...mapGetters({
+      get: 'getSkills',
+    }),
+  },
+  methods:{
+    updateSkillCategories(event){
+      //ディスパッチでstoreのアクションを呼んでいる
+      this.$store.dispatch('updateskillcategories')
+    }
+  }
+}
+</script>
